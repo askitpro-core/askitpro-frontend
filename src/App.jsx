348839634ebb8b2a8bc232099575d014e610e79a
Shivraj Nirloti <shivrajnirloti09@gmail.com>
2026-03-30 19:32:02 +0530

    Revert "Add Join Session component for student side"

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import TeacherDashboard from "./pages/TeacherDashboard";
-import JoinSession from "./components/session/JoinSession";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+
         {/* Student UI */}
         <Route path="/" element={<Home />} />
-        <Route path="/join-session" element={<JoinSession />} />
 
         {/* Teacher UI */}
         <Route path="/teacher" element={<TeacherDashboard />} />
+
       </Routes>
     </BrowserRouter>
   );
